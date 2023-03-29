5572bf898fa66ecb762aea2c6afff49658152ff4
earthjeffone <34739321+earthjeffone@users.noreply.github.com>
2023-03-29 10:46:13 -0700

    SEO snippet update

--- src/App.js
+++ src/App.js
@@ -54,7 +54,7 @@ if (window.location.host.includes("localhost")) {
   appName = window.location.host.split(".")[0];
 }
 
-document.title =
+document.title = "Portl Live Streaming";
   process.env.REACT_APP_TITLE || `${appName}'s ${document.title}`;
 
 // TODO: remove now that there are options to change to portrait
